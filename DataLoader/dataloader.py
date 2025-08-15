8024f105173ebca546737507f6cf12ffaf654014
kcjiang-upenn <145043550+kcjiang-upenn@users.noreply.github.com>
2025-08-15 12:09:04 -0400

    Add files via upload
    Added updated dataloader.py file

--- DataLoader/dataloader.py
+++ DataLoader/dataloader.py
@@ -1,3 +1,9 @@
+import os
+import pickle
+import urllib.request
+
+import datasets
+import folktables
 
 import torch
 import numpy as np
@@ -9,8 +15,6 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 def arrays_to_tensor(X, Y, Z, XZ, device):
     return torch.FloatTensor(X).to(device), torch.FloatTensor(Y).to(device), torch.FloatTensor(Z).to(
         device), torch.FloatTensor(XZ).to(device)
-
-
 
 
 def adult(data_root, display=False):
@@ -158,7 +162,122 @@ def lawschool():
     data = rawdata[['LSAT', 'GPA', 'Gender', 'resident','White','admit']]
     return data
 
+def acsincome():
+    sensitive_attr = 'SEX'
+    target = 'PINCP'
+    features = [
+        'AGEP', 'COW', 'SCHL', 'MAR', 'OCCP', 'POBP', 'RELP', 'WKHP'
+    ]
+    categories = {
+        "COW": {
+            1.0: ("Employee of a private for-profit company or"
+                  "business, or of an individual, for wages,"
+                  "salary, or commissions"),
+            2.0: ("Employee of a private not-for-profit, tax-exempt,"
+                  "or charitable organization"),
+            3.0:
+                "Local government employee (city, county, etc.)",
+            4.0:
+                "State government employee",
+            5.0:
+                "Federal government employee",
+            6.0: ("Self-employed in own not incorporated business,"
+                  "professional practice, or farm"),
+            7.0: ("Self-employed in own incorporated business,"
+                  "professional practice or farm"),
+            8.0:
+                "Working without pay in family business or farm",
+            9.0:
+                "Unemployed and last worked 5 years ago or earlier or never worked",
+        },
+        "SCHL": {
+            1.0: "No schooling completed",
+            2.0: "Nursery school, preschool",
+            3.0: "Kindergarten",
+            4.0: "Grade 1",
+            5.0: "Grade 2",
+            6.0: "Grade 3",
+            7.0: "Grade 4",
+            8.0: "Grade 5",
+            9.0: "Grade 6",
+            10.0: "Grade 7",
+            11.0: "Grade 8",
+            12.0: "Grade 9",
+            13.0: "Grade 10",
+            14.0: "Grade 11",
+            15.0: "12th grade - no diploma",
+            16.0: "Regular high school diploma",
+            17.0: "GED or alternative credential",
+            18.0: "Some college, but less than 1 year",
+            19.0: "1 or more years of college credit, no degree",
+            20.0: "Associate's degree",
+            21.0: "Bachelor's degree",
+            22.0: "Master's degree",
+            23.0: "Professional degree beyond a bachelor's degree",
+            24.0: "Doctorate degree",
+        },
+        "MAR": {
+            1.0: "Married",
+            2.0: "Widowed",
+            3.0: "Divorced",
+            4.0: "Separated",
+            5.0: "Never married or under 15 years old",
+        },
+        "SEX": {
+            1.0: "Male",
+            2.0: "Female"
+        },
+        "RAC1P": {
+            1.0: "White alone",
+            2.0: "Black or African American alone",
+            3.0: "American Indian alone",
+            4.0: "Alaska Native alone",
+            5.0: ("American Indian and Alaska Native tribes specified;"
+                  "or American Indian or Alaska Native,"
+                  "not specified and no other"),
+            6.0: "Asian alone",
+            7.0: "Native Hawaiian and Other Pacific Islander alone",
+            8.0: "Some Other Race alone",
+            9.0: "Two or More Races",
+        },
+    }
 
+    # load raw ACS data
+    get_data_fn = lambda: folktables.ACSDataSource(
+        survey_year='2018',
+        horizon='1-Year',
+        survey='person',
+    ).get_data(download=True)
+    raw = cache_dataset(f"./data/acsincome/raw_dataset.pkl", get_data_fn)
+    df = folktables.adult_filter(raw)
+
+    label_transform = lambda x: (x > 50000).astype(int)
+    X_df, y_arr, z_arr = folktables.BasicProblem(
+        features=features + [sensitive_attr],
+        target=target,
+        target_transform=label_transform,
+        group=sensitive_attr,
+        postprocess=lambda x: np.nan_to_num(x, -1),
+    ).df_to_pandas(df, categories=categories, dummies=True)
+
+    y = pd.Series(y_arr.squeeze(), name=target)
+    z = pd.Series((z_arr.squeeze() - 1).astype(int), name=sensitive_attr)
+    data = pd.concat([X_df, y, z], axis=1)
+    #data.to_csv("data/acsincome/acsincome")
+    #print("SAVED TO ACSIncome CSV")
+    return data
+
+def cache_dataset(path, get_data_fn):
+    if os.path.exists(path):
+      with open(path, "rb") as f:
+        data = pickle.load(f)
+    else:
+      directory = os.path.dirname(path)
+      os.makedirs(directory, exist_ok=True)
+      data = get_data_fn()
+      with open(path, "wb") as f:
+        pickle.dump(data, f)
+    return data
 
 
 class CustomDataset():
@@ -186,6 +305,8 @@ class FairnessDataset():
             self.get_compas_data(seed)
         elif self.dataset == 'Lawschool':
             self.get_lawschool_data(seed)
+        elif self.dataset == 'ACSIncome':
+            self.get_acsincome_data(seed)
         else:
             raise ValueError('Your argument {} for dataset name is invalid.'.format(self.dataset))
         self.prepare_ndarray()
@@ -250,9 +371,6 @@ class FairnessDataset():
         self.Y_test_ = pd.Series(self.Y_test_, name='two_year_recid')
 
 
-
-
-
     def get_lawschool_data(self,seed):
         alldata = lawschool()
         training_set, testing_set = train_test_split(alldata, test_size=0.3, random_state=seed)
@@ -266,6 +384,20 @@ class FairnessDataset():
         self.Z_test_ = testing_set['White']
         self.X_train_ = training_set.drop(['White','admit'],axis=1)
         self.X_test_ = testing_set.drop(['White','admit'],axis=1)
+        
+    def get_acsincome_data(self, seed):
+        alldata = acsincome()                            
+        train_df, test_df = train_test_split(alldata, test_size=0.3, random_state=seed)
+        train_df.index = np.arange(len(train_df))
+        test_df.index  = np.arange(len(test_df))
+
+        self.Y_train_ = train_df['PINCP']
+        self.Z_train_ = train_df['SEX']
+        self.Y_test_  = test_df['PINCP']
+        self.Z_test_  = test_df['SEX']
+
+        self.X_train_ = train_df.drop(['PINCP', 'SEX'], axis=1)
+        self.X_test_  = test_df.drop (['PINCP', 'SEX'], axis=1)
 
     def prepare_ndarray(self):
         self.normalized = False
